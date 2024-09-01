5977f5a2cbf9f74392100ca1a3bbdbf40a5a1c36
Aurelia Sindhu <100260518+AureliaSindhu@users.noreply.github.com>
2024-08-31 18:54:36 -0700

    page for wildhackle

--- frontend/.svelte-kit/generated/client/app.js
+++ frontend/.svelte-kit/generated/client/app.js
@@ -6,7 +6,8 @@ export const nodes = [
 	() => import('./nodes/2'),
 	() => import('./nodes/3'),
 	() => import('./nodes/4'),
-	() => import('./nodes/5')
+	() => import('./nodes/5'),
+	() => import('./nodes/6')
 ];
 
 export const server_loads = [];
@@ -14,8 +15,9 @@ export const server_loads = [];
 export const dictionary = {
 		"/": [2],
 		"/about": [3],
-		"/sverdle": [~4],
-		"/sverdle/how-to-play": [5]
+		"/detection": [4],
+		"/wildhackle": [~5],
+		"/wildhackle/how-to-play": [6]
 	};
 
 export const hooks = {
---
+++ frontend/.svelte-kit/generated/client/nodes/6.js
@@ -0,0 +1,3 @@
+import * as universal from "../../../../src/routes/wildhackle/how-to-play/+page.js";
+export { universal };
+export { default as component } from "../../../../src/routes/wildhackle/how-to-play/+page.svelte";
