1d85a9574e9f84297d77c9cb627a5d8d6504589e
Aurelisa Sindhunirmala <aurelisajuans@gmail.com>
2024-09-07 08:02:29 -0700

    updated backend

--- api/wildguard/models.py
+++ api/wildguard/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class UploadedImage(models.Model):
-    image = models.ImageField(upload_to='media/')
+    image = models.ImageField(upload_to='.')
     uploaded_at = models.DateTimeField(auto_now_add=True)
     predictions = models.TextField(null=True, blank=True)
